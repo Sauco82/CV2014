1d6cc6925bff7f5f9a65796c9768b342c3c3e783
Rafa Latorre <sauco82@gmail.com>
2014-05-24 18:31:35 +0200

    added some animations for sections

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,39 @@
 $(function(){
 
-	// Link navigation
+	animateNavigation();
+	animateSections();
+	
+});
+
+function animateSections() {
+	var sections = $('.section');
+
+	sections.addClass('inactive');
+
+	$(window).scroll(function(){
+
+		if ( sections.length ) {
+			sections.each(function(){			
+				if ( $(window).scrollTop() > ($(this).offset().top - 400) ) {
+					$(this).removeClass('inactive');
+				}
+			});
+			sections = $('.inactive');
+		}
+
+	})
+}
+
+function animateNavigation() {	
 	$('a').click(function() {
 		var destination = this.getAttribute('href');
+		event.preventDefault();
 
 		if ( destination && destination[0] == "#" ) {
 			scrollTo(destination)
 		}
 	});
-
-	// Scroll related events
-	$(window).scroll(function(){
-		// console.log($(window).scrollTop());
-	})
-});
-
+}
 
 function scrollTo(id) {
 	var scroll = $(id).offset().top;
