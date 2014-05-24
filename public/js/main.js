1ce6570f20f767e0c0b69011b9d4d25f9f33c4cd
Rafa Latorre <sauco82@gmail.com>
2014-05-24 16:05:23 +0200

    js interaction

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,18 @@
+$(function(){
 
+	$('a').click(function() {
+		var destination = this.getAttribute('href');
+
+		if ( destination && destination[0] == "#" ) {
+			scrollTo(destination)
+		}
+	});
+	
+});
+
+
+function scrollTo(id) {
+	$('html, body').animate({
+	    scrollTop: $(id).offset().top
+	 }, 600);
+}
