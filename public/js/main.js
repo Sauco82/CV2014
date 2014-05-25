0737e50945b9104cd21b79a4990c24866cb6c515
Rafa Latorre <sauco82@gmail.com>
2014-05-25 16:29:21 +0200

    added label

--- public/js/main.js
+++ public/js/main.js
@@ -112,7 +112,16 @@ function skillsetChart(skills) {
 			.attr("y", function(s) { return barHeight(s.value); })
 			.attr("height", function(s) { return height - barHeight(s.value); })
 			.attr("width", barWidth.rangeBand());
-	  
+	
+	chart.append("g")
+			.attr("class", "y axis")
+		.call(yAxis)
+			.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("y", 6)
+			.attr("dy", ".71em")
+			.style("text-anchor", "end")
+			.text("Power");
 }
 
 
